9a228823422b82448b7370ab75881bbb5632109c
Lusmft <teirsoul@gmail.com>
2022-03-24 21:14:45 +0000

    Adds dependency

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy'
     ),
 
-    install_requires=('coincurve>=4.3.0', 'requests'),
+    install_requires=('coincurve>=4.3.0', 'requests', 'pycoingecko'),
     extras_require={
         'cli': ('appdirs', 'click', 'privy', 'tinydb'),
         'cache': ('lmdb', ),
